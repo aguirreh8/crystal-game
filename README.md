9872d16ef1dcdb7634b5ddbac571ef658d840db0
unknown <aguirreh8@gmail.com>
2018-03-11 15:12:53 -0400

    Added commenting to code, rewrite README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# week4-game
-Crystal Game made with JQuery
+# Crystal Guessing Game
+Each crystal has a unique value. Find out how much each is worth and try to meet the goal bofore you pass it.
+
+![Crystal Game](./assets/images/preview.png)
+
+## How to play
+Click [here](https://aguirreh8.github.io/crystal-game/) to start play, or clone this repo to play on your desktop.
+
+## Author
+Henry Aguirre
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// Create global variables
 let currentNum = 0;
 let currentScore = 0;
 let redVal = 0;
@@ -7,6 +8,7 @@ let yellowVal = 0;
 let wins = 0;
 let losses = 0;
 
+// Run game initiation, reset scores and goal value
 function initializeGame() {
 	currentScore = 0;
 	$('#current').html(currentScore);
@@ -14,11 +16,13 @@ function initializeGame() {
 	setCrystalVal();
 }
 
+// Set goal value at random from 19 to 120
 function pickNum() {
 	currentNum = Math.floor((Math.random() * 101) + 20);
 	$('#goal').html(currentNum);
 }
 
+// Set value to increase with for each element at random from 1 to 12
 function setCrystalVal() {
 	redVal = Math.floor((Math.random() * 12) + 1);
 	blueVal = Math.floor((Math.random() * 12) + 1);
@@ -27,25 +31,32 @@ function setCrystalVal() {
 	// console.log('red:' + redVal + ' blue:' + blueVal + ' green:' + greenVal + ' yellow:' + yellowVal);
 }
 
+// Check current score compared to total score
 function scoreChecker() {
+	//If user's score matches goal, add 1 to wins, display it on dom, reset game
 	if (currentScore === currentNum) {
 		wins++;
 		alert("You win!");
 		$('#wins').html(wins);
 		initializeGame();
-	} else if (currentScore > currentNum) {
+	}
+	// if user's score is greater than goal, add 1 to losses, display it on dom, reset game 
+	else if (currentScore > currentNum) {
 		losses++;
 		alert('You loose..');
 		$('#losses').html(losses);
 		initializeGame();
-	} else {
+	} 
+	// else continue current game
+	else {
 		return;
 	}
 }
 
+// Run when document has finished loading
 $(document).ready(function() {
 	initializeGame();
-
+	// Add value of each element to current user's score when clicked
 	$('#red').on('click', function() {
 		currentScore = currentScore + redVal;
 		$('#current').html(currentScore);
